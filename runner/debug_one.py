2d54394218cbe070f9b4fbe88b3bac137c89605e
Stefanos Baziotis <sdi1600105@di.uoa.gr>
2023-05-14 21:14:49 -0500

    [Runner] Update debug_one.py

--- runner/debug_one.py
+++ runner/debug_one.py
@@ -3,8 +3,9 @@ import run_nb
 enable_rewriter = False
 with_modin = True
 less_replication = True
+measure_modin_mem = True
 
 succ = run_nb.run_nb(
   "<notebooks root>/<user>/<nb>",
-  enable_rewriter, with_modin, less_replication)
+  enable_rewriter, with_modin, less_replication, measure_modin_mem)
 assert succ

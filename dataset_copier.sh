9dd292e2f7a5006e3ea1fbde3d230d841ba94e1f
damitha <damitha2@illinois.edu>
2023-11-04 17:54:02 -0500

    Fix bug in copier

--- dataset_copier.sh
+++ dataset_copier.sh
@@ -49,19 +49,19 @@ nbs=(
   "qnqfbqfqo/electric-vehicle-landscape-in-washington-state"
   "xokent/cyber-security-attack-eda"
   "josecode1/billionaires-statistics-2023"
-  "mathewvondersaar/analysis-of-student-performance",
-  "viviktpharale/house-price-prediction-eda-linear-ridge-lasso",
-  "jagangupta/stop-the-s-toxic-comments-eda",
-  "kimtaehun/simple-preprocessing-for-time-series-prediction",
-  "vatsalmavani/music-recommendation-system-using-spotify-dataset",
-  "tanulsingh077/twitter-sentiment-extaction-analysis-eda-and-model",
-  "corazzon/how-to-use-pandas-filter-in-survey-eda",
-  "robikscube/big-data-bowl-comprehensive-eda-with-pandas",
-  "vbmokin/automatic-eda-with-pandas-profiling-2-9-09-2020",
-  "korfanakis/housing-in-london-eda-with-pandas-and-gif",
-  "natigmamishov/eda-with-pandas-on-telecom-churn-dataset",
-  "olgaberezovsky/eda-using-python-pandas",
-  "macespinoza/simple-eda-with-python-pandas-data-avocado-paltas",
+  "mathewvondersaar/analysis-of-student-performance"
+  "viviktpharale/house-price-prediction-eda-linear-ridge-lasso"
+  "jagangupta/stop-the-s-toxic-comments-eda"
+  "kimtaehun/simple-preprocessing-for-time-series-prediction"
+  "vatsalmavani/music-recommendation-system-using-spotify-dataset"
+  "tanulsingh077/twitter-sentiment-extaction-analysis-eda-and-model"
+  "corazzon/how-to-use-pandas-filter-in-survey-eda"
+  "robikscube/big-data-bowl-comprehensive-eda-with-pandas"
+  "vbmokin/automatic-eda-with-pandas-profiling-2-9-09-2020"
+  "korfanakis/housing-in-london-eda-with-pandas-and-gif"
+  "natigmamishov/eda-with-pandas-on-telecom-churn-dataset"
+  "olgaberezovsky/eda-using-python-pandas"
+  "macespinoza/simple-eda-with-python-pandas-data-avocado-paltas"
   "kenjee/titanic-project-example"
 )
 
